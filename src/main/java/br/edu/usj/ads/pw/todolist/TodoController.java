f808066bbb8df570c445ec1fb8556ec2ed49af5c
Osmar da Cunha Filho <Osmar da Cunha Filho>
2021-10-08 23:04:55 -0300

    organizando

--- src/main/java/br/edu/usj/ads/pw/todolist/TodoController.java
+++ src/main/java/br/edu/usj/ads/pw/todolist/TodoController.java
@@ -1,22 +1,15 @@
 package br.edu.usj.ads.pw.todolist;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.GetMapping;
-
-
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class TodoController {
-
-    // List<String> list = new ArrayList<>();
-
     @Autowired
     TodoRepository todoRepository;
 
